da915d37a82051398e6dab7a2cb26656724c2ebc
diriho <don_destin_iriho@brown.edu>
2026-03-06 10:46:14 -0500

    Update file paths in main.py to relative references

--- playground/main.py
+++ playground/main.py
@@ -5,8 +5,8 @@ from bert_probs_pseudocode import get_next_word_probability_distribution, get_sp
 
 def main():
     # Step 1: Parse the CSV file to get human-generated masked data
-    file_path = '/Users/dondestiniriho/Desktop/BLT lab/next-word-prediction/data/peelle_data/cloze_data.csv'  # Update this path to your actual CSV file
-    output_path = '/Users/dondestiniriho/Desktop/BLT lab/next-word-prediction/data/bert_data/bert_cloze_data.csv'  # Path to save the BERT results
+    file_path = '../data/peelle_data/cloze_data.csv'  # Update this path to your actual CSV file
+    output_path = '../data/bert_data/bert_cloze_data.csv'  # Path to save the BERT results
     peelle_data = csv_parser.parse_csv(file_path)
     masked_words, sentences = csv_parser.get_human_masked_data(peelle_data)
 
