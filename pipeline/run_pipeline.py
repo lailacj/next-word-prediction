1e3bd632c990da1aa6b0707e2370ed4a24f1c77f
diriho <don_destin_iriho@brown.edu>
2026-06-10 15:16:55 -0400

    feat: run the llm for all of the datasets

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -21,10 +21,10 @@ def get_list_words_given_sentence(my_list):
 # main function to run the pipeline
 def main():
     # load the data in the peelee data cloze dataset
-    file_path = PROJECT_ROOT / "data" / "parsed_data" / "michaelov_2024.csv"
+    file_path = PROJECT_ROOT / "data" / "parsed_data" / "szewczyk_2022.csv"
     masked, sentences = load_cloze_data(file_path)
 
-    model = LlamaModel()
+    model = DeepSeekModel()
     for idx, sentence in enumerate(sentences, start=1):
         word_list = get_list_words_given_sentence(masked[str(idx)])
         sentence_token_ids = model.tokenize_sentense(sentence)
